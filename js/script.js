5698b1acb8fe92419125e452e2dd35eec9eb9fcf
macbook <macbook@macbooks-MacBook-Air.local>
2016-10-23 16:11:34 +0530

    added changes

--- js/script.js
+++ js/script.js
@@ -26,36 +26,65 @@
 // })
 
 
-$('.more-products').click(function(){
+$(document).ready(function() {
+ 
+      $("#owl-demo").owlCarousel({
+     
+          autoPlay: 3000, //Set AutoPlay to 3 seconds
+     
+          items : 4,
+          itemsDesktop : [1199,3],
+          itemsDesktopSmall : [979,3]
+     
+      });
 
-	$("#more-products").slideDown( "slow", function() {});
 
-})
+    $('.more-products').click(function(){
+
+        $("#more-products").slideDown( "slow", function() {});
+
+    })
    /**
      * EFECTO PARA FLECHAS EN ACORDEON
      */
     
-$(document).on('show','.accordion', function (e) {
-     //$('.accordion-heading i').toggleClass(' ');
-     $(e.target).prev('.accordion-heading').addClass('accordion-opened');
+    $(document).on('show','.accordion', function (e) {
+         //$('.accordion-heading i').toggleClass(' ');
+         $(e.target).prev('.accordion-heading').addClass('accordion-opened');
+    });
+
+    $(document).on('hide','.accordion', function (e) {
+        $(this).find('.accordion-heading').not($(e.target)).removeClass('accordion-opened');
+        //$('.accordion-heading i').toggleClass('fa-chevron-right fa-chevron-down');
+    });
+
+
+    // $('#close-more-product').click(function(){
+
+    //  $("#more-product").slideUp( "slow", function() {});
+
+    // })
+
+    // $(".thumbnails").hover(function(){
+
+    //     alert("hello");
+    // })
+
+    $("#owl-demo").owlCarousel({
+     
+          autoPlay: 3000, //Set AutoPlay to 3 seconds
+     
+          items : 4,
+          itemsDesktop : [1199,3],
+          itemsDesktopSmall : [979,3]
+     
+    });
+ 
 });
 
-$(document).on('hide','.accordion', function (e) {
-    $(this).find('.accordion-heading').not($(e.target)).removeClass('accordion-opened');
-    //$('.accordion-heading i').toggleClass('fa-chevron-right fa-chevron-down');
-});
 
 
-// $('#close-more-product').click(function(){
 
-// 	$("#more-product").slideUp( "slow", function() {});
-
-// })
-
-// $(".thumbnails").hover(function(){
-
-//     alert("hello");
-// })
 
 function hovered(id,cat_id){
 
